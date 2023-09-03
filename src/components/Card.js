1b506b9b2b59216c227eb59fef544c0b508c3db6
Purice Victor <pvictor0110@gmail.com>
2023-09-03 19:55:29 +0300

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,12 +11,19 @@ export default function Card(props) {
 
     return (
         <div className="card">
-            {badgeText && <div className="card--badge">SOLD OUT</div>}
-            <img src={`../images/${props.img}`} className="card--image" />
+            {
+                badgeText && 
+                <div className="card--badge">SOLD OUT</div>
+            }
+            <img 
+            src={`../images/${props.coverImg}`} 
+            className="card--image" 
+            />
+
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
             </div>
             <p className="card--title">{props.title}</p>
